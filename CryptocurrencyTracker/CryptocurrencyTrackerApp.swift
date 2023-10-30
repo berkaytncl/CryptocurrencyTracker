5d4335414b62b7e77d464540a0533ac9566def6c
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-30 22:25:42 +0300

    Initial Commit

---
+++ CryptocurrencyTracker/CryptocurrencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyTrackerApp.swift
+//  CryptocurrencyTracker
+//
+//  Created by Berkay Tuncel on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
