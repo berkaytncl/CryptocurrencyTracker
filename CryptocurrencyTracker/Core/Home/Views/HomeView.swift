b8ac9973225426bd850c8f68d3749df7d2cc6eb9
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-31 16:01:52 +0300

    Create HomeView and a custom navigation header

---
+++ CryptocurrencyTracker/Core/Home/Views/HomeView.swift
@@ -0,0 +1,62 @@
+//
+//  HomeView.swift
+//  CryptocurrencyTracker
+//
+//  Created by Berkay Tuncel on 31.10.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        HomeView()
+            .toolbar(.hidden)
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.theme.accent)
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
--- CryptocurrencyTracker/CryptocurrencyTrackerApp.swift
+++ CryptocurrencyTracker/CryptocurrencyTrackerApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CryptocurrencyTrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                HomeView()
+                    .toolbar(.hidden)
+            }
         }
     }
 }
