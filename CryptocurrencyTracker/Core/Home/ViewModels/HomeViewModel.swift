cc3086980fe15f48ce629949501031ce1aedf46e
Berkay Tuncel <berkaytncl98@gmail.com>
2023-11-06 16:06:44 +0300

    Filter data by search bar text using Combine

--- CryptocurrencyTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptocurrencyTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,10 +23,25 @@ final class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] returnedCoins in
-                self?.allCoins = returnedCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoin in
+                self?.allCoins = returnedCoin
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else { return coins }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
